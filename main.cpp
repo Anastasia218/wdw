b037f41ef43dd579b0438b20d612dae6568c6672
Anastasia218 <nastikpost@mail.ru>
2021-11-06 14:21:20 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    std::vector<std::string> v1 = {"Mia", "Anastasia", "cat", "dog"};
+    v1.push_back("Sweet");
+    for (std::string s: v1){
+        std::cout << s << std::endl;
+    }
+
+}
